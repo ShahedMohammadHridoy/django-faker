6af0d84cf0b4f882cedf65903a86d48a5d2825cc
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-12-19 03:54:30 +0600

    model

--- myfaker/models.py
+++ myfaker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
+    enrollment_date = models.DateField()
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
